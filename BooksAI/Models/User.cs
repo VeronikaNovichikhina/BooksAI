ee2c554215f0e277187acf086cbb744177218636
Katherine <lipchanskayakatya@gmail.com>
2025-04-13 15:08:03 +0500

    //

--- BooksAI/Models/User.cs
+++ BooksAI/Models/User.cs
@@ -1,10 +1,14 @@
-﻿namespace BooksAI.Models
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace BooksAI.Models
 {
-    public class User
+    public class User 
     {
         public int Id { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
+
+        public string Role { get; set; } = "User";
 
         public bool IsAdmin => Email == "admin@mail.com";//вставите почту админа
     }
--- BooksAI/Program.cs
+++ BooksAI/Program.cs
@@ -1,13 +1,12 @@
-
 using BooksAI.Data;
-using Microsoft.AspNetCore.Authentication.Cookies;
+using BooksAI.Models;
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using System;
 
 var builder = WebApplication.CreateBuilder(args);
 
-
+// Настройка сервисов
 builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
 
 var connectionString = builder.Configuration.GetConnectionString("MSSQL");
@@ -16,8 +15,6 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString);
 });
 builder.Services.AddSession();
-
-
 
 var app = builder.Build();
 
@@ -28,6 +25,25 @@ app.MapStaticAssets();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+using (var scope = app.Services.CreateScope())
+{
+    var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+
+    if (!context.Users.Any(u => u.Email == "admin@example.com"))
+    {
+        var passwordHasher = new PasswordHasher<User>();  
+        var hashedPassword = passwordHasher.HashPassword(null, "Admin123");  // Хешируем пароль
+
+        context.Users.Add(new User
+        {
+            Email = "admin@example.com",
+            Password = hashedPassword, 
+            Role = "Admin"
+        });
+        context.SaveChanges();
+    }
+}
+
 app.Run();
-
-
